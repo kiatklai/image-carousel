ff3e059c8ec49310c71827e22d18bf11ab52cac0
kiatklai <likhitthaworn.k@gmail.com>
2023-08-06 20:56:38 +0900

    イベント発火

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import ImageData from "./ImageData"
 import { AiOutlineArrowLeft,AiOutlineArrowRight } from "react-icons/ai";
 
 const ImageSlider=()=>{
+  const [current,setCurrent] = useState(0)
+
+  const prevSlide=()=>{
+    setCurrent(current-1)
+  }
+  const nextSlide=()=>{
+    setCurrent(current+1)
+  }
+
   return(
     <section className="slider">
-      <AiOutlineArrowLeft className="leftArrow"/>
-      <AiOutlineArrowRight className="rightArrow"/>
+      <AiOutlineArrowLeft className="leftArrow" onClick={prevSlide}/>
+      <AiOutlineArrowRight className="rightArrow" onClick={nextSlide}/>
       {ImageData.map((data,index)=>{
         return(
-          <div className="slide" key={index}>
+          <div className={index===current ? "slide active":"slide"} key={index}>
             <div>
               <img src={data.image} alt={data.title} className="image"/>
               <p>{data.title}</p>
