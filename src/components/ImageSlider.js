6465f2500a647ddff6cc4190022afc58ffa93426
kiatklai <likhitthaworn.k@gmail.com>
2023-08-06 20:28:17 +0900

    アイコン導入

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,8 +1,11 @@
 import ImageData from "./ImageData"
+import { AiOutlineArrowLeft,AiOutlineArrowRight } from "react-icons/ai";
 
 const ImageSlider=()=>{
   return(
     <section className="slider">
+      <AiOutlineArrowLeft className="leftArrow"/>
+      <AiOutlineArrowRight className="rightArrow"/>
       {ImageData.map((data,index)=>{
         return(
           <div className="slide" key={index}>
